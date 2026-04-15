49225b8609ccbe56cbbe7fa9507c13befc720ed7
Downing Nightingale IV <downingndec4@gmail.com>
2026-04-15 16:30:20 -0400

    JS for aboutUs

---
+++ js/about.js
@@ -0,0 +1,15 @@
+function pup1() {
+    document.getElementById("storyPup").textContent = "Boe the ___ ";
+    document.getElementById("storyDesc").textContent = "test";
+}
+
+function pup2() {
+    document.getElementById("storyPup").textContent = "Buster the ___";
+    document.getElementById("storyDesc").textContent = "test";
+}
+
+function pup3() {
+    document.getElementById("storyPup").textContent = "Snowball the Pomerian";
+    document.getElementById("storyImg").src="../images/storyPup1-removebg-preview.png";
+    document.getElementById("storyDesc").textContent = "test";
+}
