d51ced05946ff0e69e0cedd767c3cf0a7ca7e7eb
Downing Nightingale IV <downingndec4@gmail.com>
2026-04-15 20:53:38 -0400

    Revise pup details and images in about.js
    Updated pup descriptions and images for better storytelling.

--- js/about.js
+++ js/about.js
@@ -1,15 +1,17 @@
 function pup1() {
-    document.getElementById("storyPup").textContent = "Boe the ___ ";
+    document.getElementById("storyPup").textContent = "TBD";
+    document.getElementById("storyImg").src="../images/storyPup1.png";
     document.getElementById("storyDesc").textContent = "test";
 }
 
 function pup2() {
-    document.getElementById("storyPup").textContent = "Buster the ___";
-    document.getElementById("storyDesc").textContent = "test";
+    document.getElementById("storyPup").textContent = "Buster the Mop";
+    document.getElementById("storyImg").src="../images/storyPup2.png";
+    document.getElementById("storyDesc").textContent = "Buster is a 6 year old Komondor. He is extremely active and loves to go to the park to play with other dogs. He was originally given to us by a family who could not take care of him anymore to rehome. After just a few weeks of being at the shelter, he was adopted by a volunteer who assists at Adopt a Puppy Jacksonville and now takes great care of him.";
 }
 
 function pup3() {
     document.getElementById("storyPup").textContent = "Snowball the Pomerian";
-    document.getElementById("storyImg").src="../images/storyPup1-removebg-preview.png";
-    document.getElementById("storyDesc").textContent = "test";
+    document.getElementById("storyImg").src="../images/storyPup3.png";
+    document.getElementById("storyDesc").textContent = "Snowball is a 4 year old Pomerian. He is extremely friendly and loves to cuddle with anyone who will give him attention. He was originally found in a poor neighborhood and was rescued by a friendly volunteer. He has now been adopted by a loving family and is happyily cuddling with his brand new family.";
 }
