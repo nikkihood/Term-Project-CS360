48c73a0ba3f2b14ed2983e7d13b5a38ce6da8199
Downing Nightingale IV <downingndec4@gmail.com>
2026-04-18 11:27:32 -0400

    Add new pup stories

--- js/about.js
+++ js/about.js
@@ -18,3 +18,17 @@ function pup3() {
     document.getElementById("storyImg").alt="Snowball the Pomerian Image";
     document.getElementById("storyDesc").textContent = "Snowball is a 4 year old Pomerian. He is extremely friendly and loves to cuddle with anyone who will give him attention. He was originally found in a poor neighborhood and was rescued by a friendly volunteer. He has now been adopted by a loving family and is happyily cuddling with his brand new family.";
 }
+
+function pup4() {
+    document.getElementById("storyPup").textContent = "Sally the Spots";
+    document.getElementById("storyImg").src="../images/storyPup4.png";
+    document.getElementById("storyImg").alt="Sally the Spots Image";
+    document.getElementById("storyDesc").textContent = "Sally is a 3 year old Dalmation. She is loves attention from any person that she meets and enjoys eating lots of food. She was abandoned by her mother after birth and was living near a local gas station until a local decided to take her in. Now she has found a new loving home where she is known to be quite spoiled but is an extremely happy pup.";
+
+}
+function pup5() {
+    document.getElementById("storyPup").textContent = "Boe the Glutton";
+    document.getElementById("storyImg").src="../images/storyPup5.png";
+    document.getElementById("storyImg").alt="Boe the Glutton Image";
+    document.getElementById("storyDesc").textContent = "Boe is a 5 year old Pitbull. He is very hyper when it comes to being around any person or dog and loves to eat plenty of food. He was given to us by a family who could no longer deal with him due to moving to another country where the home they moved into didn't allow dogs. However, he luckily found a new and loving family who takes him on morning runs and gives him plenty of love and snacks.";
+}
