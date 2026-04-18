06c7857af284d7169e034711ae1263b7770b1fbe
Downing Nightingale IV <downingndec4@gmail.com>
2026-04-18 11:36:58 -0400

    Add new event 'Pup Water Works' and related images

--- js/newsEvents.js
+++ js/newsEvents.js
@@ -6,27 +6,32 @@ window.addEventListener("load", function() {
 
 const eventTitles = [
     "Jacksonville Dogo Yoga Day",
-    "Jacksonville Pup Walk"
+    "Jacksonville Pup Walk",
+    "Pup Water Works"
 ];
 
 const eventImages = [
     "../images/newsEvent1.avif",
-    "../images/newsEvent2.avif"
+    "../images/newsEvent2.avif",
+    "../images/newsEvent3.avif"
 ];
 
 const eventDescriptions = [
     "Join us for the Jacksonville Dogo Yoga Day! We will be hosting a day where you can bring your dog and friends to a yoga session to do yoga that is specifically designed for you and your dog! This event is to increase the bond between you and your pup alongside being able to meet other puppies and dog owners in the community.",
-    "Jacksonville Pup Walk is a great place to go on a walk with you puppies! This event is a pup walk in the park where you and your pup go on a 1.5 mile walk in the park with other dogs. This is a great way to get out of the house and spend some time with your pup while also getting some exercise in!"
+    "Jacksonville Pup Walk is a great place to go on a walk with you puppies! This event is a pup walk in the park where you and your pup go on a 1.5 mile walk in the park with other dogs. This is a great way to get out of the house and spend some time with your pup while also getting some exercise in!",
+    "Pup Water Works is an event that will make your pup have an amazing time if they enjoy swimming. This event allows for you and your pup to go to a waterpark for a day with plenty of other dog owners in the area! This event will help you be able to meet new community members while also going out for a swim."
 ];
 
 const eventImageTitles = [
     "Dogo Yoga Day",
-    "Pup Walk"
+    "Pup Walk",
+    "Pup Water Works"
 ];
 
 const eventImageAlts = [
     "Jacksonville Dogo Yoga Day Image",
-    "Jacksonville Pup Walk Image"
+    "Jacksonville Pup Walk Image",
+    "Pup Water Works Image"
 ];
 
 let index = 0;
@@ -59,9 +64,9 @@ function nextEvent() {
 }
 
 function increaseOpacity() {
-    document.querySelector("#eventPhotoArea figcaption").style.opacity = "0.8";
+    document.querySelector("#eventPhotoArea figcaption").style.opacity = 0.8;
 }
 
 function decreaseOpacity() {
-    document.querySelector("#eventPhotoArea figcaption").style.opacity = "0";
+    document.querySelector("#eventPhotoArea figcaption").style.opacity = 0;
 }
